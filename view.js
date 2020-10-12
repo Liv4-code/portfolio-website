4f19f44ba89a88394c9f23fa5358d195c864d329
livi <scholtz.ice@gmail.com>
2020-10-12 21:45:23 +0200

    Added other two blocks, added heading, and styled block two and three.

--- view.js
+++ view.js
@@ -1,1 +1,6 @@
 // Add mouseover event for block 1 nav icons giving CV description.
+
+const arrow = document.querySelector(.arrow-container span);
+console.log(arrow);
+
+arrow.addEventListener("", function());
