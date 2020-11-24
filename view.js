06e9ec14d2ae8601f62bec3f8f8bbedc33289304
livi <scholtz.ice@gmail.com>
2020-11-24 12:37:50 +0200

    Added styling to form in block 3.

--- view.js
+++ view.js
@@ -5,3 +5,13 @@ const homeButton = document.querySelector("button");
 homeButton.addEventListener("click", () => {
     window.scrollTo(0,0);
 });
+
+const form = document.querySelector("form");
+const formChildren = form.children;
+
+// console.log(Array.from(formChildren));
+
+Array.from(formChildren).forEach(child => {
+    child.style.marginTop = "15px";
+});
+
