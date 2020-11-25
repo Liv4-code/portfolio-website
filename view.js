31a406b3436d77ce4deebdf0dd876f87bcf4e4e6
livi <scholtz.ice@gmail.com>
2020-11-25 09:45:58 +0200

    Created submit event in form for regex validation in firstname and lastname input fields.

--- view.js
+++ view.js
@@ -1,17 +1,39 @@
 // Add mouseover event for block 1 nav icons giving CV description.
 
+// Form Styling
+
+const form = document.querySelector("form");
+const formChildren = form.children;
+
+Array.from(formChildren).forEach(child => {
+    child.style.marginTop = "15px";
+});
+
+// Home Button
+
 const homeButton = document.querySelector("button");
 
 homeButton.addEventListener("click", () => {
     window.scrollTo(0,0);
 });
 
-const form = document.querySelector("form");
-const formChildren = form.children;
+// Submit event on form:
 
-// console.log(Array.from(formChildren));
+form.addEventListener("submit", e => {
+    e.preventDefault();
 
-Array.from(formChildren).forEach(child => {
-    child.style.marginTop = "15px";
+    // validation
+
+    const pattern = /^[a-zA-z]{2,}$/
+    const firstname = form.firstname.value;
+    const lastname = form.lastname.value;
+
+    if(pattern.test(firstname) && pattern.test(lastname)){
+        alert("Yes, that password is valid");
+    } else {
+        alert("No, your password is not valid!");
+    }
 });
+
+// User input while they type into field (keyboard event):
 
